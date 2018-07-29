bedd37d77c13ac135a10835bba15b0874a1f60a6
Sam Grace <sam@festicket.com>
2018-07-29 13:01:18 +0100

    tidy up all code

--- main.mjs
+++ main.mjs
@@ -1,17 +1,9 @@
-// import _ from 'lodash';
 const _ = require("lodash");
-
-console.table([{ 0: 1, 1: 2 }, { 0: 45, 2: 12 }, { 0: 111, 1: 93 }]);
-
-console.table([[1, 2], [45, , 12], [111, 93]]);
-
-const blockHeight = 2;
-const blockWidth = 2;
 
 const blockSize = 2;
 
 // prettier-ignore
-const startState = [
+const incompleteState = [
   [, , , 2,],
   [, 2, , ,],
   [, , 4, ,],
@@ -19,92 +11,44 @@ const startState = [
 ];
 
 // prettier-ignore
-const tempState = [
+const completePerfectState = [
   [1,2,3,4,],
   [4,3,2,1,],
   [3,4,1,2,],
   [2,1,4,3,]
 ];
 
-const completeLine = _.range(1, startState.length + 1);
+// prettier-ignore
+const completeErrorState = [
+  [4,2,3,4,],
+  [1,3,2,1,],
+  [3,4,1,2,],
+  [2,1,4,3,]
+];
 
-console.table(startState);
+function multiSplit(arr, n) {
+  const arrays = Array(n)
+    .fill()
+    .map(() => Array());
 
-function isFinished(state) {
-  if (typeof state === "object") return state.every(x => isFinished(x));
-  else return state;
-}
-
-console.log(isFinished([[1, 2]]));
-console.log(isFinished(startState));
-//
-// function rows(state) {
-//   return state.values()
-// }
-
-for (let row of tempState) {
-  console.log(row);
-}
-
-console.log();
-
-for (let col of _.unzip(tempState)) {
-  console.log(col);
-}
-
-function* rows(state) {
-  for (let row of state) {
-    yield row;
+  for (var i = 0; i < n; i++) {
+    for (var index = i; index < arr.length; index += n) {
+      arrays[i].push(arr[index]);
+    }
   }
+
+  return arrays;
 }
 
-for (let row of rows(tempState)) {
-  const x = _.difference(row, completeLine).length === 0;
-  console.log(x);
-}
-
-function isComplete(row) {
-  console.log("is comp", row);
-  // return _.isEmpty(_.xor(row, completeLine));
-  return _(row)
-    .xor(completeLine)
-    .isEmpty();
-}
-
-function transpose(array) {
-  return _.unzip(array);
-}
-
-// let areRowsGood = _.every(tempState, isComplete);
-// let areColsGood = _.every(transpose(tempState), isComplete);
-// console.log(areRowsGood, 'temp state');
-// console.log(areColsGood, 'temp state');
-//
-// areRowsGood = _.every(startState, isComplete);
-// areColsGood = _.every(transpose(startState), isComplete);
-// console.log(areRowsGood, 'start state');
-// console.log(areColsGood, 'start state');
-
-console.log(_.every([1, 2, 3]), 12321);
-
-for (let row of tempState) {
-  console.log(_.chunk(row, 2));
-}
-
-// for (const [i, row] of tempState.entries()) {
-//   console.log("%d: %s", i, row);
-//   console.log(_.chunk(row, 2));
-// }
-
-function areBlocksGood(rows) {
+function getBlocks(rows, blockSize) {
   let i = 0;
   const flattenedRowChunks = _.chain(rows)
     .map(row => _.chunk(row, blockSize))
     .flatten()
     .value();
 
-  const nthPartitionedRowChunks = multiSplit(flattenedRowChunks, blockSize)
-  const flattened = _.flatten(nthPartitionedRowChunks)
+  const nthPartitionedRowChunks = multiSplit(flattenedRowChunks, blockSize);
+  const flattened = _.flatten(nthPartitionedRowChunks);
 
   const blocks = [];
   while (!_(flattened).isEmpty()) {
@@ -115,32 +59,42 @@ function areBlocksGood(rows) {
         .value()
     );
   }
-
   return blocks;
 }
 
-for (let ss of areBlocksGood(tempState)) {
-  console.log(ss);
-}
+function check(state, blockSize, label) {
+  const completeLine = _.range(1, Math.pow(blockSize, 2) + 1);
 
-for (let ss of areBlocksGood(tempState)) {
-  console.log(ss);
-}
-
-const asd = _.every(areBlocksGood(tempState), isComplete);
-console.log(asd);
-
-
-function multiSplit(arr, n) {
-  const arrays = Array(n).fill().map(() => Array())
-
-  for (var i = 0; i < n; i++) {
-    for (var index = i; index < arr.length; index+=n) {
-      arrays[i].push(arr[index]);
-    }
+  function isComplete(row) {
+    return _(row)
+      .xor(completeLine)
+      .isEmpty();
   }
 
-  console.log('rrewrew', arrays)
+  function transpose(array) {
+    return _.unzip(array);
+  }
 
-  return arrays;
+  console.log(`checking ${label}:`);
+  console.log(`block size: ${blockSize}:`);
+  console.table(state);
+
+  const doBlocksValidate = _.every(getBlocks(state, blockSize), isComplete);
+  const doRowsValidate = _.every(state, isComplete);
+  const doColsValidate = _.every(transpose(state), isComplete);
+  const isValidSudoku = _.every([
+    doBlocksValidate,
+    doRowsValidate,
+    doColsValidate
+  ]);
+
+  console.log("Are all blocks good?", doBlocksValidate);
+  console.log("Are all rows good?", doRowsValidate);
+  console.log("Are all cols good?", doColsValidate);
+  console.log("Is Sodoku solved?", isValidSudoku);
+  console.log();
 }
+
+check(incompleteState, blockSize, "incompleteState");
+check(completePerfectState, blockSize, "completePerfectState");
+check(completeErrorState, blockSize, "completeErrorState");
